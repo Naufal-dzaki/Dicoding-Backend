83702fdcdcfd3c5cbc3885b3250a554091a9c215
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-05-09 10:37:19 +0700

    init hapi framework

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,10 +1,14 @@
+/**
+ * hapi server menerima satu parameter yaitu server option yang merupakan objek konfigurasi, kita dapat menetapkan  properti port dan host
+ * server.start() dijalankan secara asynchronous, kita bisa menggunakan async await
+ */
 const hapi = require("@hapi/hapi");
 
 const init = async () => {
   const server = hapi.server({
     port: 5000,
     host: "localhost",
-  });
+  }); //server option
 
   await server.start();
   console.log(`server bejalan pada ${server.info.uri}`);
