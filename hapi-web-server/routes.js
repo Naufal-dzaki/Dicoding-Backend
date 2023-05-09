023d420c0c3a89ab67ae045fefda83a33d0eda2b
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-05-09 10:56:38 +0700

    method/verb request & routing

---
+++ hapi-web-server/routes.js
@@ -0,0 +1,44 @@
+/**
+ * method yang memiliki nilai '*' berarti route dapat diakses dengan semua method yang ada pada http
+ * path yang memiliki nilai '/{any*}' berfungsi untuk menangani permintaan masuk pada path yang belum di daftarkan
+ */
+
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "home page";
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "about page";
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "halaman tidak ditemukan";
+    },
+  },
+];
+
+module.exports = routes;
--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -1,14 +1,18 @@
 /**
  * hapi server menerima satu parameter yaitu server option yang merupakan objek konfigurasi, kita dapat menetapkan  properti port dan host
  * server.start() dijalankan secara asynchronous, kita bisa menggunakan async await
+ * Routing pada HAPI dapat dilakukan pada method server.route()
  */
 const hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
   const server = hapi.server({
     port: 5000,
     host: "localhost",
   }); //server option
+
+  server.route(routes);
 
   await server.start();
   console.log(`server bejalan pada ${server.info.uri}`);
