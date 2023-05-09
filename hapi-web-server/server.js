9aebae7b04315c12f43e74ee047c0b4b10e01917
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-05-09 14:47:29 +0700

    path parameter

--- hapi-web-server/server.js
+++ hapi-web-server/server.js
@@ -2,6 +2,10 @@
  * hapi server menerima satu parameter yaitu server option yang merupakan objek konfigurasi, kita dapat menetapkan  properti port dan host
  * server.start() dijalankan secara asynchronous, kita bisa menggunakan async await
  * Routing pada HAPI dapat dilakukan pada method server.route()
+ * path dalam routing bisa di artikan alamat yang digunakan client untuk melakukan request
+ * untuk menggunakan path parameter pada hapi menggunakan '{}' -> /users/{username}
+ * untuk menggunankan path parameter sebagai optional kita bisa menggunakan '?' -> /users/{username?}
+ * optional path parameter hanya bisa digunakan pada akhir path saja -> /users/{username}/posts/{title?}
  */
 const hapi = require("@hapi/hapi");
 const routes = require("./routes");
