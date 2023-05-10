ce9f4528c98307fbfcd25ac83ccdef42c4742527
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-05-10 16:26:07 +0700

    response toolkit

--- hapi-web-server/routes.js
+++ hapi-web-server/routes.js
@@ -1,6 +1,8 @@
 /**
  * method yang memiliki nilai '*' berarti route dapat diakses dengan semua method yang ada pada http
  * path yang memiliki nilai '/{any*}' berfungsi untuk menangani permintaan masuk pada path yang belum di daftarkan
+ * pada handler terdapat value h (inisial dari hapi), h dalam parameter disebut dengan response toolkit yang fungsinya hampir sama dengan response pada nodejs native
+ * h.response sama dengan response.end dan .code sama dengan response.statusCode
  */
 
 const routes = [
@@ -8,14 +10,20 @@ const routes = [
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "home page";
+      return h.response("Home page").code(200);
     },
   },
   {
     method: "*",
     path: "/",
     handler: (request, h) => {
-      return "halaman tidak dapat diakses dengan method tersebut";
+      const response = h.response(
+        "halaman tidak dapat di akses dengan method tersebut"
+      );
+      response.type("text/plain");
+      response.header("X-Powered-By", "nodejs");
+      response.code(400);
+      return response;
     },
   },
   {
